d4a87aa68d374eb6577c347300e51a10c55d5160
Ivy Minaywa <mwirukii@gmail.com>
2017-10-18 11:54:49 +0300

    modified c3

--- c3.py
+++ c3.py
@@ -66,5 +66,5 @@ class Cus_dvd(db.Entity):
     PrimaryKey(customers, dvds)
 
 
-
-db.generate_mapping()
+db.bind("postgres", host="localhost", user="ivy", database="ivy", password="password")
+db.generate_mapping(create_tables = True)
